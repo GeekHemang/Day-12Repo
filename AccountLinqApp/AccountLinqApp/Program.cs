6888e815f438103e1e31220fcb2fc5121ada967d
GeekHemang <garghemang@gmail.com>
2025-12-01 14:55:40 +0530

    Day 12

--- AccountLinqApp/AccountLinqApp/Program.cs
+++ AccountLinqApp/AccountLinqApp/Program.cs
@@ -10,13 +10,51 @@ namespace AccountLinqApp
         static void Main(string[] args)
         {
 
-
-            CaseStudy1();
+            CaseStudy3();
+            //CaseStudy2();
+            //CaseStudy1();
 
             //TODO: Display all account holders alphatically
 
         }
+        private static void CaseStudy2()
+        {
+            List<Account> accounts = new List<Account>();
 
+            accounts.Add(new Account(101, "Kannan", 1000));
+            accounts.Add(new Account(102, "Hemang", 2000));
+            accounts.Add(new Account(103, "Ashish", 3000));
+            accounts.Add(new Account(104, "Preeti", 4000));
+
+
+            //write linq query to get the names in descending order.
+
+            var accountnamequerry = accounts.OrderByDescending(a => a.Name);
+            var accountnamesZtoA = accountnamequerry.ToList();
+            accountnamesZtoA.ForEach(MyPrintDetails);
+
+        }
+        private static void CaseStudy3()
+        {
+            List<Account> accounts = new List<Account>();
+
+            accounts.Add(new Account(101, "Kannan", 1000));
+            accounts.Add(new Account(102, "Hemang", 2000));
+            accounts.Add(new Account(103, "Ashish", 3000));
+            accounts.Add(new Account(104, "Preeti", 4000));
+
+            var namesWithI = accounts.
+                Where(a => a.Name.Contains('i'))
+                .ToList();
+            namesWithI.ForEach(a=> Console.WriteLine(a.Name));
+
+            var balancegreaterthan3000 = accounts.
+                Where(a => a.Balance > 3000)
+                .ToList();
+            balancegreaterthan3000.ForEach(a => Console.WriteLine(a.Name));
+
+
+        }
         private static void CaseStudy1()
         {
             List<Account> accounts = new List<Account>();
@@ -27,8 +65,14 @@ namespace AccountLinqApp
             accounts.Add(new Account(104, "Preeti", 4000));
 
 
-            //write linq query to get the top 2 richesh acc holders
+            //write linq query to get the names in descending order.
 
+            var accountnamequerry = accounts.OrderByDescending(a => a.Name);
+            var accountnamesZtoA = accountnamequerry.ToList();
+            accountnamesZtoA.ForEach(MyPrintDetails);
+
+
+            //write linq query to get the top 2 richesh acc holders
             var orderedAccountQuery = accounts.OrderByDescending(MySortingAlgorithm);//this differed
 
             var top2query = orderedAccountQuery.Take(3);
